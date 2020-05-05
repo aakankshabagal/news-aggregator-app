b7e4e289dc0ba2fa64790c5c18fbf874cac11015
aakankshabagal <61360507+aakankshabagal@users.noreply.github.com>
2020-05-05 23:53:13 +0530

    newsagg

---
+++ src/scripts/index.js
@@ -0,0 +1,87 @@
+
+
+const apikey='9485e5925b36466aaa9269821cb75b31';
+let article=document.getElementById("news-articles");
+
+function start(){
+    console.log("onload");//comment to onload start function
+    document.getElementById("search").addEventListener('keypress',searchvalue);
+    retrive();
+
+}
+async function retrive(searchvalueText="")
+{
+    article.innerHTML='<p class="newsload">News loading please wait..</p>';
+    
+    if(searchvalueText!="")
+    {
+        url=`https://newsapi.org/v2/top-headlines?q=${searchvalueText}&apiKey=${apikey}`;
+    }
+    else{
+        url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=${apikey}`;
+    }
+    const responce=await fetch(url);
+
+    const result=await responce.json();
+
+    getNews(result);
+}
+
+
+//for passing value to retrive function
+
+async function searchvalue(event){
+
+    if(event.which==13 || event.keycode==13 ||  event.key=="Enter")
+    {
+        console.log(event);
+        console.log(event.which);
+        console.log(event.key);
+        console.log(event.keycode);
+        retrive(event.target.value);
+
+
+        console.log(event.target.value);
+
+    }
+};
+
+
+//let article=document.getElementById("news-articles");
+
+function getNews(news)
+{
+    let output="";
+    if(news.totalResults>0)
+    {
+
+    news.articles.forEach(ind  => {
+        
+   
+     output+=
+    `<section class="container rubberBand" >
+    <li class="article animated pulse delay-2s"><a class="article-link" href="${ind.url}">
+
+    <div class="img">
+    <img src="${ind.urlToImage}" class="article-img" alt="${ind.title}" />
+    </div>
+
+    <h2 class="article-title">${ind.title}</h2>
+
+    <p class="article-description" >${ind.description || "Description not available."}</p>
+
+    <span class="article-author">-${ind.author?ind.author:"unknown"}</span><br>
+    </a>
+    </li>
+    </section>`;
+});
+
+article.innerHTML=output;
+    
+
+}
+else{
+
+    article.innerHTML=`<li class="not-found" >No article was found based on the search..</li>`
+}
+};
