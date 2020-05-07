c32a6cffcbda3d27a6faddfee645c598f7a0401a
aakankshabagal <61360507+aakankshabagal@users.noreply.github.com>
2020-05-07 15:59:54 +0530

    news

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,12 +7,12 @@ let article=document.getElementById("news-articles");
 function start(){
     console.log("onload");//comment to onload start function
     document.getElementById("search").addEventListener('keypress',searchvalue);
-    retrive();
+    retrieve();
 
 }
-async function retrive(searchvalueText="")
+async function retrieve(searchvalueText="")
 {
-    article.innerHTML='<p class="newsload">News loading please wait..</p>';
+    article_area.innerHTML='<p class="newsload">News loading please wait..</p>';
     
     if(searchvalueText!="")
     {
@@ -33,13 +33,13 @@ async function retrive(searchvalueText="")
 
 async function searchvalue(event){
 
-    if(event.which==13 || event.keycode==13 ||  event.key=="Enter")
+    if(event.which===13 || event.keycode===13 ||  event.key==="Enter")
     {
         console.log(event);
         console.log(event.which);
         console.log(event.key);
         console.log(event.keycode);
-        retrive(event.target.value);
+        retrieve(event.target.value);
 
 
         console.log(event.target.value);
@@ -61,19 +61,19 @@ function getNews(news)
    
      output+=
     `<section class="container  animated pulse delay-2s" >
-    <li class="article"><a class="article-link" href="${ind.url}">
+        <li class="article"><a class="article-link" href="${ind.url}">
 
-    <div class="img">
-    <img src="${ind.urlToImage}" class="article-img" alt="${ind.title}" />
-    </div>
+            <div class="img">
+            <img src="${ind.urlToImage}" class="article-img" alt="${ind.title}"/>
+            </div>
 
-    <h2 class="article-title">${ind.title}</h2>
+            <h2 class="article-title">${ind.title}</h2>
 
-    <p class="article-description">${ind.description || "Description not available."}</p>
+            <p class="article-description">${ind.description || "Description not available."}</p>
 
-    <span class="article-author">${ind.author?ind.author:"unknown"}</span><br>
-    </a>
-    </li>
+            <span class="article-author">${ind.author?ind.author:"unknown"}</span><br>
+            </a>
+        </li>
     </section>`;
 });
 
