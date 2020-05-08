8134abf048ba9862e7f88b92b91a13658f74ca21
aakankshabagal <61360507+aakankshabagal@users.noreply.github.com>
2020-05-08 19:46:09 +0530

    comment1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,47 +1,6 @@
 const apiKey='9485e5925b36466aaa9269821cb75b31';
 let article_area=document.getElementById("news-articles");
 
-
-async function retrieve(searchValueText="")
-{
-    article_area.innerHTML='<p class="newsload">News loading please wait..</p>';
-    
-    if(searchValueText!="")
-    {
-        url=`https://newsapi.org/v2/everything?q=${searchValueText}&apiKey=${apiKey}`;
-    }
-    else{
-        url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}`;
-    }
-    const responce=await fetch(url);
-
-    const result=await responce.json();
-
-    getNews(result);
-};
-
-
-//for passing value to retrive function
-
-async function searchvalue(event){
-
-    if(event.which===13 || event.keycode===13 ||  event.key==="Enter")
-    {
-        console.log(event);
-        console.log(event.which);
-        console.log(event.key);
-        console.log(event.keycode);
-        retrieve(event.target.value);
-
-
-        console.log(event.target.value);
-
-    }
-};
-
-
-//let article=document.getElementById("news-articles");
-
 function getNews(news)
 {
     let output="";
@@ -77,6 +36,47 @@ else{
     article_area.innerHTML=`<h1  class="not-found">No article was found based on the search.</h1>`
 }
 }
+
+async function retrieve(searchValueText="")
+{
+    article_area.innerHTML='<p class="newsload">News loading please wait..</p>';
+    
+    if(searchValueText!="")
+    {
+        url=`https://newsapi.org/v2/everything?q=${searchValueText}&apiKey=${apiKey}`;
+    }
+    else{
+        url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}`;
+    }
+    const responce= await fetch(url);
+
+    const result=await responce.json();
+
+    getNews(result);
+};
+
+
+//for passing value to retrive function
+
+async function searchvalue(event){
+
+    if(event.which===13 || event.keycode===13 ||  event.key==="Enter")
+    {
+        console.log(event);
+        console.log(event.which);
+        console.log(event.key);
+        console.log(event.keycode);
+        retrieve(event.target.value);
+
+
+        console.log(event.target.value);
+
+    }
+};
+
+
+//let article=document.getElementById("news-articles");
+
 
 function start()
 {
